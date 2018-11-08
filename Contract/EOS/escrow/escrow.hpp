5427ccfad684c6d7951422c4098b86b1e706eae8
咚咚 <lf2kexe@gmail.com>
2018-11-09 00:45:32 +0800

    Update st_transfer & onTransfer

--- Contract/EOS/escrow/escrow.hpp
+++ Contract/EOS/escrow/escrow.hpp
@@ -1,4 +1,7 @@
-﻿
+﻿/**
+ *  @dev minakokojima, yukiexe
+ */
+
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
 #include <cmath>
@@ -10,8 +13,26 @@
 using namespace eosio;
 using namespace std;
 
-typedef double real_type;
 
+struct st_transfer {
+    account_name from;
+    account_name to;
+    asset        quantity;
+    string       memo;
+
+    EOSLIB_SERIALIZE( st_transfer, (from)(to)(quantity)(memo) )
+};
+
+uint64_t string_to_price(string s) {
+    uint64_t z = 0;
+    for (int i=0;i<s.size();++i) {
+        if ('0' <= s[i] && s[i] <= '9') {
+            z *= 10; 
+            z += s[i] - '0';
+        }
+    }
+    return z;
+} // string_to_price()
 class escrow : public contract
 {
 public:
@@ -19,14 +40,41 @@ public:
         contract(self) {
     }
     
-    void onTransfer(account_name from, account_name to,
-                    extended_asset quantity, string memo);        
+    void onTransfer(account_name from, account_name to, extended_asset quantity, std::string memo) {        
+    
+        if (to != _self) return;
+    
+        require_auth(from);
+        eosio_assert(quantity.is_valid(), "invalid token transfer");
+        eosio_assert(quantity.amount > 0, "must transfer a positive amount");
+    
+        asset a = asset(quantity.symbol, quantity.amount / 2);
+        asset b = asset(quantity.symbol, quantity.amount - quantity.amount / 2);
+
+        if (a.amount > 0) {
+            action(
+                permission_level{_self, N(active)},
+                quantity.countract, N(transfer),
+                make_tuple(_self, N(minakokojima), a,
+                std::string(""))
+            ).send();
+        }
+        if (b.amount > 0) {
+            action(
+                permission_level{_self, N(active)},
+                quantity.countract, N(transfer),
+                make_tuple(_self, N(rukamoemoe51), b,
+                std::string(""))
+            ).send();
+        }
+    } // onTransfer()
+
 
     void apply(account_name code, action_name action) {
         auto &thiscontract = *this;
 
         if (action == N(transfer)) {
-            auto transfer_data = unpack_action_data<currency::transfer>();
+            auto transfer_data = unpack_action_data<st_transfer>();
             onTransfer(transfer_data.from, transfer_data.to, extended_asset(transfer_data.quantity, code), transfer_data.memo);
             return;
         }
