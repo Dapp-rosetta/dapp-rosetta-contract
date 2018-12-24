cabfc5ad95bfbd5f5fe907b4fea304f8159d2233
flyq <flyq951@gmail.com>
2018-12-24 14:40:55 +0800

    fix null abi & del excess file

--- Contract/EOS/counter/counter.cpp
+++ Contract/EOS/counter/counter.cpp
@@ -1,26 +1,22 @@
 #include <eosiolib/eosio.hpp>
-#include <eosiolib/print.hpp>
 #include <eosiolib/singleton.hpp>
 using namespace eosio;
-using namespace std;
 
-CONTRACT counter_contract : public eosio::contract {
+CONTRACT counter : public contract {
 public:
-    counter_contract(name receiver, name code, datastream<const char*> ds): 
-        contract(receiver, code, ds),
-        todos(receiver, receiver.value) {
-    }
+    counter(name receiver, name code, datastream<const char*> ds)
+        :contract(receiver, code, ds), todos(receiver, receiver.value) {}
 
     ACTION init() {
         require_auth(_self);
         todos.set(todo{}, _self);   
     }
-    
+
     ACTION add(name author) {
         auto itr = todos.get();
         itr.counter += 1;
         todos.set(itr, _self);
-        eosio::print("Now counter is ", itr.counter);
+        print("Now counter is ", itr.counter);
     }
 
     TABLE todo {
@@ -31,5 +27,5 @@ public:
     todo_table todos;
 };
 
-EOSIO_DISPATCH( counter_contract, (add)(init) )
+EOSIO_DISPATCH( counter, (init)(add) )
 
