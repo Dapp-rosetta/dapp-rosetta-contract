6dfcecccc9a0456d48930e7f460210c4626ebfbb
lychees <lychees67@gmail.com>
2019-05-02 19:53:22 +0800

    Update npotokendemo

--- Contract/EOS/npotokendemo/npotokendemo.cpp
+++ Contract/EOS/npotokendemo/npotokendemo.cpp
@@ -1,9 +1,10 @@
 ﻿/**
- *  @dev minakokojima, rukamoemoe51
+ *  @dev minakokojima
  */
 
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
+#include <eosiolib/singleton.hpp>
 #include <string>
 
 using namespace eosio;
@@ -16,35 +17,35 @@ struct st_transfer {
     string memo;
 };
 
-CONTRACT escrow : public contract {
+CONTRACT npotokendemo : public contract {
 public:
-    escrow(name receiver, name code, datastream<const char*> ds):
-        contract(receiver, code, ds) {
+    npotokendemo(name receiver, name code, datastream<const char*> ds):
+        contract(receiver, code, ds), connector(receiver, receiver.value) {
     }
 
     ACTION transfer(name from, name to, asset quantity, string memo);
 
-    void take_out(name from, extended_asset out) {
+    // 取錢
+    void take_out(name from, extended_asset out) {        
         if (from != _self) return;
         auto itr = connector.get();
-        delta = itr.supply / itr.balance * in.amout;
-        itr.balance -= out.amount;
+        itr.balance -= out.quantity;
 
         action(
             permission_level{_self, "active"_n},
             out.contract, name("transfer"),
-            make_tuple(_self, from, out.amount,
+            make_tuple(_self, from, out.quantity,
             string("take out some eos"))
         ).send();
     }
 
-    void put_in(name from, extented_asset in) {
+    // 存錢
+    void put_in(name from, extended_asset in) {        
         auto itr = connector.get();
-        delta = itr.supply / itr.balance * in.amout;
-        itr.balance += in.amount;
+        auto delta = asset(itr.supply * in.quantity.amount / itr.balance, itr.supply.symbol);
+        itr.balance += in.quantity;
         itr.supply += delta;
-        connector.set(itr, _self);
-
+        connector.set(itr, _self);    
         action(
             permission_level{_self, "active"_n},
             _self, name("transfer"),
@@ -54,13 +55,13 @@ public:
     }
 
     void onTransfer(name from, name to, extended_asset in, string memo) {
-        if (to != _self) return;
+        if (to != _self) return;        
         require_auth(from);
         eosio_assert(in.quantity.is_valid(), "invalid token transfer");
         eosio_assert(in.quantity.amount > 0, "must transfer a positive amount");
-        eosio_assert(in.quantity.symbol == "EOS 4"_n, "only allow eos");
+//        eosio_assert(in.quantity.symbol == "EOS 4"_n, "only allow eos");
         eosio_assert(in.contract == "eosio.token"_n, "only allow eos");
-        put_in(from, in);
+        put_in(from, in); 
     } // onTransfer()
 
 
@@ -69,7 +70,7 @@ public:
         asset balance;
     };
 
-    typedef singleton<"connector_t"_n, connector_t> connector_table;
+    typedef singleton<"connector"_n, connector_t> connector_table;
     connector_table connector;
 
     void apply(uint64_t receiver, uint64_t code, uint64_t action) {
@@ -84,7 +85,7 @@ public:
 
 extern "C" {
     [[noreturn]] void apply(uint64_t receiver, uint64_t code, uint64_t action) {
-        escrow p( name(receiver), name(code), datastream<const char*>(nullptr, 0) );
+        npotokendemo p( name(receiver), name(code), datastream<const char*>(nullptr, 0) );
         p.apply(receiver, code, action);
         eosio_exit(0);
     }
